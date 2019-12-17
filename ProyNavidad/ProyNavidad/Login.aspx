0c366bdb26e0f19947e8c28b4710e1c42da05614
Ibán Juarros Eguren <ibanjuarros@segundo.edu>
2019-12-17 11:35:56 +0100

    Hemos creado el proyecto y añadido el diseño del login

---
+++ ProyNavidad/ProyNavidad/Login.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="ProyNavidad.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtUsuario" runat="server"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtPass" type="password" placeholder="password" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="lblError" runat="server" Text="Label" BorderColor="#FFFF99" Visible="False"></asp:Label>
+            <br />
+            <asp:Button ID="ButtonLogin" runat="server" Text="Iniciar Sesion" OnClick="Button1_Click" />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ProyNavidad/ProyNavidad/Login.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace ProyNavidad
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        SqlConnection cn = new SqlConnection("Data Source=SEGUNDO150;Initial Catalog=AVAN_iban;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            string consulta = "select count(*) from navi.Login where Usuario= @usuario and Password = @password";
+            SqlCommand cm = new SqlCommand(consulta, cn);
+            cm.Parameters.AddWithValue("@usuario", txtUsuario.Text);
+            cm.Parameters.AddWithValue("@password", txtPass.Text);
+
+            //Si existe un usuario con los datos retorna true, caso contrario false
+            bool correcto = Convert.ToInt32(cm.ExecuteScalar()) > 0;
+
+            if (correcto)
+            {
+                //Si existe, Bienvenido...
+                Response.Redirect("");
+
+            }
+            else
+            {
+                lblError.Text = "Usuario o contraseña incorrecta";
+                lblError.Visible = true;
+                txtUsuario.Text = "";
+                txtPass.Text = "";
+                txtUsuario.Focus();
+
+            }
+            cn.Close();
+
+        }
+    }
+}
